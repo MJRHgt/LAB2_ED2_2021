6c568284e47dee95beafcdca7d639b378ca9279c
jamg1906 <jamg1906@gmail.com>
2021-09-10 10:34:09 -0600

    Heap & node added

---
+++ DataStructures/Nodo.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataStructures
+{
+    class Nodo<T>
+    {
+        public Nodo<T> Padre;
+        public Nodo<T> Hijoizq;
+        public Nodo<T> Hijoder;
+        public T Valor;
+    }
+}
